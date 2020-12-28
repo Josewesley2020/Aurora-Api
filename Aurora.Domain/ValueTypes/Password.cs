a1b6e5fa50dfc8a806b098e30e08d7c328e0b94e
Jose Wesley <jwes.ofc1994@gmail.com>
2020-12-28 14:16:15 -0300

    Mudando Regras de dominio para validação de senha e nome

---
+++ Aurora.Domain/ValueTypes/Password.cs
@@ -0,0 +1,53 @@
+﻿using Flunt.Validations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aurora.Domain.ValueTypes
+{
+    public struct Password
+    {
+        private readonly string _value;
+        public readonly Contract contract;
+
+        private Password(string value)
+        {
+            _value = value;
+            contract = new Contract();
+            Validate();
+
+            if(contract.Valid)
+                _value = Convert.ToBase64String(new UTF8Encoding().GetBytes(_value));
+        }
+
+        public override string ToString() =>
+            _value;
+
+        public static implicit operator Password(string input) =>
+            new Password(input.Trim());
+
+        private bool Validate()
+        {
+            if (string.IsNullOrWhiteSpace(_value))
+                return AddNotification("Is necessary to inform the Password.");
+
+            if(_value.Length > 10)
+                return AddNotification("The password must be less than 10 characters.");
+
+            // if (!Regex.IsMatch(_value, (@"[A-Z]")));
+               // return AddNotification("The password cannot contain uppercase letters");
+
+            if (!Regex.IsMatch(_value, (@"[^a-zA-Z0-9]")))
+                return AddNotification("The password must contain a special character.");
+
+            return true;
+        }
+
+        private bool AddNotification(string message)
+        {
+            contract.AddNotification(nameof(Password), message);
+            return false;
+        }
+    }
+}
